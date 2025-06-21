7e27d7bbaef1dcf11bf1c77323c76532c700f33a
Kanat Sydykov <ksydykov@in.nurtelecom.kg>
2025-06-22 05:39:08 +0600

    add metadata for cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,13 @@
+import { Metadata } from 'next';
+
+import { PAGES } from '@/shared/configs';
+
 import { Cart } from './cart';
+
+export const metadata: Metadata = {
+	title: PAGES.CART.title,
+	description: PAGES.CART.description,
+};
 
 export default function Page() {
 	return <Cart />;
